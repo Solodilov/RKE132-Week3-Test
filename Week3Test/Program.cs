96dd09f2a4246615074d7adb042f1c4b533e7aa5
Ervin Solodilov <ervin.solodilov@tktk.ee>
2023-12-15 19:26:03 +0200

    add week3test code

---
+++ Week3Test/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Mis hinda sa sai?");
+string userMark = Console.ReadLine().ToLower();
+
+if (userMark == "a")
+{
+    Console.WriteLine("Suurepärane!");
+}
+else if (userMark == "b")
+{
+    Console.WriteLine("Väga hea!");
+}
+else if (userMark == "c")
+{
+    Console.WriteLine("Hea!");
+}
+else if (userMark == "d")
+{
+    Console.WriteLine("Rahuldav");
+}
+else if (userMark == "e")
+{
+    Console.WriteLine("Kasin");
+}
+else if (userMark == "f")
+{
+    Console.WriteLine("Puudulik");
+}
+else
+{
+    Console.Write("Vale väärtus");
+}
---
+++ Week3TestSwitch/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Mis hinda sa sai?");
+string userMark = Console.ReadLine().ToLower();
+
+switch (userMark)
+{
+    case "a":
+        Console.WriteLine("Suurepärane!");
+            break;
+    case "b":
+        Console.WriteLine("Väga hea!");
+        break;
+    case "c":
+        Console.WriteLine("Hea!");
+        break;
+    case "d":
+        Console.WriteLine("Rahuldav");
+        break;
+    case "e":
+        Console.WriteLine("Kasin");
+        break;
+    case "f":
+        Console.WriteLine("Puudulik");
+        break;
+    default:
+        Console.WriteLine("Vale väärtus");
+        break;
+}
